b25d6f1c9796ec607caac5f46927d89d28794163
Maaz Adil <maazadil786@gmail.com>
2020-09-16 21:28:42 -0700

    Added Names and tweets to cells from API

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -9,15 +9,37 @@
 import UIKit
 
 class HomeTableTableViewController: UITableViewController {
+    
+    var tweetArray = [NSDictionary]() //start with empty dictionariy
+    var numberOfTweet: Int!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadTweet() //when view loads calls the loadTweet finction
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+   
+    
+    func loadTweet(){
+        
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": 10]//
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in //when sucess NSDictionary repsonse we will call it tweets so when we write our code we can refer to reuslt as tweets and we will store the tweets in local array
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)  //storing tweets in tweet array
+            }
+             self.tableView.reloadData() //make sure anytime we call the API, we repopulate our list to reload data with that content
+        }, failure: { (Error) in
+            print("Could not retreive tweets! oh no!")
+        })//call AP
     }
 
     @IBAction func onLogout(_ sender: Any) {
@@ -26,17 +48,38 @@ class HomeTableTableViewController: UITableViewController {
         UserDefaults.standard.set(false, forKey: "userLoggedIn") //set value for userLoggein to false so next time it asks you to log in 
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+            //deciding what labels will display
+        
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        
+        cell.userNameLabel.text = user["name"] as? String//in user dictinary we are getting name
+        cell.tweetContent.text = tweetArray[indexPath.row]["text"] as! String //gets tweet at specific index of array of tweets. texts is what the API refers to tweets
+        //identifier of tweet cell is set to our identifier of the table view cell
+        //exclamation mark forces it to be of type TweetCellTableViewCell
+        let imageUrl = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageUrl!)
+        
+        if let imageData = data{
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        return cell
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1 //just have 1 section
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tweetArray.count//return amount of teets fetched from API
     }
+    
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
